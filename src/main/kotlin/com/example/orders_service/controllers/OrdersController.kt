ac469c88f06f67249d33ab15bae02487610a2644
luisvt <luisbigvargas@gmail.com>
2024-10-03 05:18:45 -0400

    Step 3: Store and retrieve orders

--- src/main/kotlin/com/example/orders_service/controllers/OrdersController.kt
+++ src/main/kotlin/com/example/orders_service/controllers/OrdersController.kt
@@ -1,18 +1,55 @@
 package com.example.orders_service.controllers
 
 import com.example.orders_service.models.Order
+import com.example.orders_service.repositories.OrdersRepository
 import com.example.orders_service.services.OrdersService
+import java.lang.RuntimeException
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.HttpClientErrorException.NotFound
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/orders")
 class OrdersController(
     private val ordersService: OrdersService,
+    private val ordersRepository: OrdersRepository,
 ) {
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    fun findById(@PathVariable id: Long): Order {
+
+        return ordersRepository.findById(id)
+            .orElseThrow {
+                ResponseStatusException(
+                    HttpStatus.NOT_FOUND,
+                    "Not Found"
+                )
+            }
+    }
+
+
+    @GetMapping
+    @ResponseBody
+    fun findAll(
+        @RequestParam(defaultValue = "20") size: Int,
+        @RequestParam(defaultValue = "0") page: Int
+    ): Page<Order> {
+        return ordersRepository.findAll(PageRequest.of(page, size))
+    }
+
 
     @PostMapping
     @ResponseBody
--- src/test/kotlin/com/example/orders_service/controllers/OrdersControllerTest.kt
+++ src/test/kotlin/com/example/orders_service/controllers/OrdersControllerTest.kt
@@ -2,6 +2,7 @@ package com.example.orders_service.controllers
 
 import net.javacrumbs.jsonunit.spring.jsonContent
 import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestMethodOrder
 import org.springframework.beans.factory.annotation.Autowired
@@ -9,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
 
 @SpringBootTest
@@ -19,7 +21,21 @@ class OrdersControllerTest(
 ) {
 
     @Test
-    fun `Create Order`() {
+    @Order(1)
+    fun `Get Order 1 Not Found`() {
+        mockMvc.get("/api/orders/1") {
+            accept = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isNotFound() }
+            content {
+                string("")
+            }
+        }
+    }
+
+    @Test
+    @Order(2)
+    fun `Create Order 1`() {
         mockMvc.post("/api/orders") {
             contentType = MediaType.APPLICATION_JSON
             content = """{
@@ -39,6 +55,71 @@ class OrdersControllerTest(
                             |{"product":{"name":"Apple","price":0.6000000000,
                             |"offer":"buyOneGetOneFree","id":1},"quantity":2}],
                             |"totalAmount":0.6000000000,
+                            |"createdOn":"${'$'}{json-unit.ignore}",
+                            |"id":1}""".trimMargin()
+                    )
+                }
+            }
+        }
+    }
+
+    @Test
+    @Order(3)
+    fun `Get Orders Including 1`() {
+        mockMvc.get("/api/orders") {
+            accept = MediaType.APPLICATION_JSON
+            content = """{
+  "orderLines": [
+    {
+      "product": {"id": 1},
+      "quantity": 2
+    }]
+}"""
+        }.andExpect {
+            status { isOk() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            content {
+                jsonContent {
+                    isEqualTo(
+                        """{"content":[
+                            |{"orderLines":[
+                            |{"product":{"name":"Apple","price":0.6000000000,"offer":"buyOneGetOneFree","id":1},
+                            |"quantity":2
+                            |}],
+                            |"totalAmount":0.60,
+                            |"createdOn":"${'$'}{json-unit.ignore}",
+                            |"id":1}],
+                            |"pageable":{"pageNumber":0,"pageSize":20,
+                            |"sort":{"sorted":false,"empty":true,"unsorted":true},
+                            |"offset":0,"paged":true,"unpaged":false},
+                            |"last":true,"totalPages":1,
+                            |"totalElements":1,
+                            |"size":20,"number":0,
+                            |"sort":{"sorted":false,"empty":true,"unsorted":true},
+                            |"first":true,
+                            |"numberOfElements":1,
+                            |"empty":false}""".trimMargin()
+                    )
+                }
+            }
+        }
+    }
+
+    @Test
+    @Order(4)
+    fun `Get Order 1 Found`() {
+        mockMvc.get("/api/orders/1") {
+            accept = MediaType.APPLICATION_JSON
+        }.andExpect {
+            status { isOk() }
+            content { contentType(MediaType.APPLICATION_JSON) }
+            content {
+                jsonContent {
+                    isEqualTo(
+                        """{"orderLines":[
+                            |{"product":{"name":"Apple","price":0.6000000000,
+                            |"offer":"buyOneGetOneFree","id":1},"quantity":2}],
+                            |"totalAmount":0.60,
                             |"createdOn":"${'$'}{json-unit.ignore}",
                             |"id":1}""".trimMargin()
                     )
